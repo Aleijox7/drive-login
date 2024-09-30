4d16cd7cfd7173b978b4b0f1b2bde5bd6e8daa04
aaguilar <aaguilar@omitsis.com>
2024-09-30 19:45:04 +0200

    Add open graph image and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,21 +27,23 @@ export default function RootLayout({
 
   return (
     <html lang={detectedLocale}>
-      <head>
-        <title>{siteConfig.appName}</title>
-        <meta name="description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
-        <meta property="og:url" content="https://kodi-drive.nejo.dev"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:type" content="KodiDrive"/>
-        <meta property="og:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
-        <meta property="og:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
-        <meta property="twitter:card" content=""/>
-        <meta property="twitter:title" content="KodiDrive"/>
-        <meta property="twitter:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
-        <meta property="twitter:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
-      </head>
-      <body className={inter.className}>
-        <NextIntlClientProvider locale={detectedLocale} messages={messages}>
+    <head>
+      <title>{siteConfig.appName}</title>
+      <meta name="description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content="KodiDrive"/>
+      <meta property="og:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+      <meta property="og:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
+      <meta property="og:url" content="https://kodi-drive.nejo.dev"/>
+      <meta name="twitter:site" content="@Aleijox7"/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      {/*<meta property="twitter:card" content=""/>*/}
+      <meta property="twitter:title" content="KodiDrive"/>
+      <meta property="twitter:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+      <meta property="twitter:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
+    </head>
+    <body className={inter.className}>
+    <NextIntlClientProvider locale={detectedLocale} messages={messages}>
           {children}
         </NextIntlClientProvider>
       </body>
