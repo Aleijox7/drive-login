f4e9737533a86961379f6e015922b7dcb4f5043e
aaguilar <aaguilar@omitsis.com>
2024-09-30 20:11:45 +0200

    Add open graph image and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider } from 'next-intl';
 import {siteConfig} from "@/config/siteConfig";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Kodi Oauth",
-  description: "Kodi Oauth",
-};
 
 export default function RootLayout({
   children, params: { locale }
@@ -27,24 +21,23 @@ export default function RootLayout({
 
   return (
     <html lang={detectedLocale}>
-    <head>
-      <title>{siteConfig.appName}</title>
-      <meta name="description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:title" content="KodiDrive"/>
-      <meta property="og:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
-      <meta property="og:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
-      <meta property="og:url" content="https://kodi-drive.nejo.dev"/>
-      <meta name="twitter:site" content="@Aleijox7"/>
-      <meta name="twitter:card" content="summary_large_image"/>
-      {/*<meta property="twitter:card" content=""/>*/}
-      <meta property="twitter:title" content="KodiDrive"/>
-      <meta property="twitter:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
-      <meta property="twitter:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
-    </head>
-    <body className={inter.className}>
-    <NextIntlClientProvider locale={detectedLocale} messages={messages}>
-          {children}
+      <head>
+        <title>{siteConfig.appName}</title>
+        <meta name="description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content={ siteConfig.appName }/>
+        <meta property="og:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+        <meta property="og:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
+        <meta property="og:url" content="https://kodi-drive.nejo.dev"/>
+        <meta name="twitter:site" content="@Aleijox7"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:title" content={ siteConfig.appName }/>
+        <meta property="twitter:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+        <meta property="twitter:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
+      </head>
+      <body className={inter.className}>
+        <NextIntlClientProvider locale={detectedLocale} messages={messages}>
+            {children}
         </NextIntlClientProvider>
       </body>
     </html>
