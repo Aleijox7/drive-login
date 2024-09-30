9ba8b9c97792633ca10557e7bb8efe85d62795f9
aaguilar <aaguilar@omitsis.com>
2024-09-30 19:34:05 +0200

    Add open graph image and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider } from 'next-intl';
+import {siteConfig} from "@/config/siteConfig";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,19 @@ export default function RootLayout({
 
   return (
     <html lang={detectedLocale}>
+      <head>
+        <title>{siteConfig.appName}</title>
+        <meta name="description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+        <meta property="og:url" content="https://kodi-drive.nejo.dev"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:type" content="KodiDrive"/>
+        <meta property="og:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+        <meta property="og:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
+        <meta property="twitter:card" content=""/>
+        <meta property="twitter:title" content="KodiDrive"/>
+        <meta property="twitter:description" content="Play your Google Drive media with the simplicity that Kodi offers"/>
+        <meta property="twitter:image" content="https://kodi-drive.nejo.dev/open-graph-img.png"/>
+      </head>
       <body className={inter.className}>
         <NextIntlClientProvider locale={detectedLocale} messages={messages}>
           {children}
